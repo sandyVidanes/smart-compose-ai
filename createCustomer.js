d8940cf580e03f2aac69e9f112981e860f9386ed
Imari Joy Borda <imari.borda2018@gmail.com>
2024-11-06 11:49:10 +0800

    clean

--- createCustomer.js
+++ createCustomer.js
@@ -3,16 +3,8 @@ const AWS = require('aws-sdk');
  
 module.exports.createCustomer = async (event) => {
    try {
+      console.log('test')
       const body = JSON.parse(event.body);
-      const dynamoDb = new AWS.DynamoDB.DocumentClient();
-      const putParams = {
-        TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
-        Item: {
-          primary_key: body.name,
-          email: body.email,
-        },
-      };
-      await dynamoDb.put(putParams).promise();
      
       return {
         statusCode: 201,
