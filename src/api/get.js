c0ef61a91c22159082c213e0db2d6a78882e14cb
Imari Joy Borda <imari.borda2018@gmail.com>
2024-11-06 13:00:54 +0800

    add headers

--- src/api/get.js
+++ src/api/get.js
@@ -3,6 +3,11 @@
 module.exports.handler = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': '*'
+    },
     body: JSON.stringify(
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
--- src/api/post.js
+++ src/api/post.js
@@ -7,6 +7,11 @@ module.exports.handler = async (event) => {
 
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Methods': '*'
+        },
         body: JSON.stringify({
           message: `${message} in ${style} style`  || 'something' ,
         }),
