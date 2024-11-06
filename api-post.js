21f938f37ddb447ce676a0c83405a39c980f64cb
Imari Joy Borda <imari.borda2018@gmail.com>
2024-11-06 11:59:20 +0800

    change status code

--- api-post.js
+++ api-post.js
@@ -3,14 +3,12 @@
 module.exports.handler = async (event) => {
    try {
       const body = JSON.parse(event.body);
-     
       return {
-        statusCode: 201,
+        statusCode: 200,
         body: JSON.stringify({
           message: `${body.message} in ${body.style} style`  || 'something' ,
         }),
       };
-
    } catch (e) {
       console.log(e);
    }
